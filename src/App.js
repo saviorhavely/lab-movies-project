53ff6d5e65c5c28e6365e4417e56f3bd7869f8fc
saviorhavely <saviorhavely@gmail.com>
2020-02-18 22:44:22 -0300

    :rocket: - footer implementado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 import './style.scss';
 
 const App = () => {
     return (
-        <div className="App">
+        <div id="app" className="App">
             <Header />
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './style.scss';
+
+const Footer = () => {
+
+    return(
+        <div id="main-footer">
+            <div className="container text-center">
+                Sávio Rhavely - Iniciando Estudos em React
+            </div>
+        </div>
+    );
+
+}
+
+export default Footer;
